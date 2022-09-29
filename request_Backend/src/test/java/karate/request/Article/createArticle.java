507881f72271915b5ef76d8888cd7855dd4b3fe7
William Cabrera <williamalberto018@gmail.com>
2022-09-29 11:56:28 -0500

    Se elimino el directorio request_Backend

--- request_Backend/src/test/java/karate/request/Article/createArticle.java
+++
@@ -1,9 +0,0 @@
-package karate.request.Article;
-
-import com.intuit.karate.junit5.Karate;
-
-public class createArticle {
-    @Karate.Test
-    Karate testSample(){return Karate.run("classpath:Karate/request/Article/createArticle.feature").relativeTo(getClass());
-    }
-}
--- request_Backend/src/test/java/karate/request/Article/deleteArticle.java
+++
@@ -1,9 +0,0 @@
-package karate.request.Article;
-
-import com.intuit.karate.junit5.Karate;
-
-public class deleteArticle {
-    @Karate.Test
-    Karate testSample(){return Karate.run("classpath:Karate/request/Article/DeleteArticle.feature").relativeTo(getClass());
-    }
-}
